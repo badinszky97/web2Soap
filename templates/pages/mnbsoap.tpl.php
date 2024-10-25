c5c63e0a5a2de788f7b6c367221281e9ad900b8f
HLISZU <hliszu@hallgato.uni-neumann.hu>
2024-10-25 21:36:16 +0200

    devizapárt képes lekérdezni

--- templates/pages/mnbsoap.tpl.php
+++ templates/pages/mnbsoap.tpl.php
@@ -2,7 +2,8 @@
 
 
 <form action=# method=post>
-<label for="mainapiarfolyam">Két árfolyam napi szintje:</label>
+<label for="mainapiarfolyam">Egy adott devizapár (pl. Eur-Huf, Eur-Usd, …) adott napján mennyi volt az árfolyam?</label>
+<br>
 <select name="egyik" id="mainapiarfolyam">
 <?php
     $client = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL");
@@ -28,7 +29,7 @@
     }
 ?>
 </select>
-<input type="date" name="datum">
+<input type="date" name="datum" required>
 <input type="submit" name=maiarfolyam value="Árfolyam letöltése">
 </form>
 
@@ -36,9 +37,9 @@
 <?php
 if(isset($_POST['maiarfolyam']))
 {
-
+    $devizak = $_POST['egyik'] . "," . $_POST['masik'];
     $objClient = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL", array('trace' => true));
-    $currrates = $objClient->GetExchangeRates(array('startDate' => "2015-09-25", 'endDate' => "2015-09-30", 'currencyNames' => "EUR,USD,CHF"))->GetExchangeRatesResult;
+    $currrates = $objClient->GetExchangeRates(array('startDate' => $_POST['datum'], 'endDate' => $_POST['datum'], 'currencyNames' => $devizak))->GetExchangeRatesResult;
     $dom_root = new DOMDocument();
     $dom_root->loadXML($currrates);
     
@@ -46,14 +47,14 @@ if(isset($_POST['maiarfolyam']))
     
     foreach( $searchNode as $searchNode ) {
         $date = $searchNode->getAttribute('date');
-        print $date . "\n";
+        print $date . "\n<br>";
         $rates = $searchNode->getElementsByTagName( "Rate" ); 
         foreach( $rates as $rate ) {
             print "\t" . $rate->getAttribute('unit') . " " ;
             print $rate->getAttribute('curr');
             print "  =  ";
             print $rate->nodeValue;
-            print " HUF\n";
+            print " HUF\n<br>";
         }
         echo "<br>";
     }
@@ -62,48 +63,3 @@ if(isset($_POST['maiarfolyam']))
 ?>
 
 
-
-<?php
-
-
-try {
-    $client = new SoapClient("http://www.mnb.hu/arfolyamok.asmx?WSDL");
-    echo "<br>GetCurrentExchangeRates()<br>";
-    $tömb = (array)simplexml_load_string($client->GetCurrentExchangeRates()->GetCurrentExchangeRatesResult);
-    echo $tömb['Day']['date']."<br>";
-    print_r($tömb);
-
-
-    echo "<br><br><br><br><br>GetInfo()<br>";
-    $xml = simplexml_load_string($client->GetInfo()->GetInfoResult);
-    $json = json_encode($xml);
-    $array = json_decode($json,TRUE);
-
-    echo "most:";
-    echo sizeof($array['Currencies']['Curr']);
-
-    echo "<label for=\"cars\">Choose a car:</label>";
-    echo "<select name=\"cars\" id=\"cars\" form=\"carform\">";
-    for($i=0;$i<sizeof($array['Currencies']['Curr']);$i++)
-    {
-        echo "<option value=\"". $array['Currencies']['Curr'][$i]."\">".$array['Currencies']['Curr'][$i]  ."</option>";
-    }
-        
-    echo "</select>";
-
-    //$tömb = (array)simplexml_load_string($client->GetInfo()->GetInfoResult);
-    //print(  (array)$tömb['Currencies']->Curr );
-
-
-
-    echo "<br><br><br><br><br>GetCurrencies()<br>";
-    $tömb = (array)simplexml_load_string($client->GetCurrencies()->GetCurrenciesResult);
-    print_r($tömb);
-    echo "<br><br><br><br><br>GetDateInterval()<br>";
-    $tömb = (array)simplexml_load_string($client->GetDateInterval()->GetDateIntervalResult);
-    print_r($tömb);
-    } catch (SoapFault $e) {
-    var_dump($e);
-    }
-
- ?>
